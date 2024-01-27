f11ccc2c3064a316258b72f7055f9a6b0a81e11a
YuliaKravchukS <ljuljek554@gmail.com>
2024-01-27 11:27:36 +0200

    1-timer +

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,34 +1,40 @@
+
 'use strict';
 
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const button = document.querySelector('[data-start]');
 const timer = document.querySelector('.timer');
+const input = document.querySelector('#datetime-picker');
 const valueDays = document.querySelector('[data-days]');
 const valueHours = document.querySelector('[data-hours]');
 const valueMinutes = document.querySelector('[data-minutes]');
 const valueSeconds = document.querySelector('[data-seconds]');
 
-
-
 let userSelectedDate = new Date('2020-01-24');
 let diff = 0;
+let intervalId;
+
+button.disabled = true;
 
 function onButtonClick() {
   diff = userSelectedDate.getTime() - new Date().getTime();
-    if (diff < 0) {
-      button.setAttribute("disabled", true);
-      window.alert("Please choose a date in the future");
-    } else {
-      button.removeAttribute("disabled");
-      const intervalId = setInterval(() => {
+  if (diff >= 0) {
+    button.disabled = false;
+    intervalId = setInterval(() => {
+      button.disabled = true;
+      input.disabled = true;
+      diff -= 1000;
       tickTack(convertMs(diff));
+      if (diff < 1000) {
+        clearInterval(intervalId);
+      }
     }, 1000);
-      
-    }
-  
-};
+  }
+}
 
 const options = {
   enableTime: true,
@@ -37,10 +43,21 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     userSelectedDate = selectedDates[0];
+    if (userSelectedDate < Date.now()) {
+      iziToast.show({
+        message: 'Please choose a date in the future',
+        messageColor: '#FFFFFF',
+        backgroundColor: '#B51B1B',
+        position: 'topRight',
+      });
+    } else {
+      button.disabled = false;
+    }
   },
+ 
 };
 
-const fp = flatpickr("#datetime-picker", options);
+flatpickr(input, options);
 
 const convertMs = ms => {
   const second = 1000;
@@ -61,9 +78,12 @@ function tickTack({ days, hours, minutes, seconds }) {
   valueHours.textContent = `${addLeadingZero(hours)}`;
   valueMinutes.textContent = `${addLeadingZero(minutes)}`;
   valueSeconds.textContent = `${addLeadingZero(seconds)}`;
-};
+}
 
 function addLeadingZero(num) {
   return num.toString().padStart(2, '0');
-};
+}
+
+
 button.addEventListener('click', onButtonClick);
+
